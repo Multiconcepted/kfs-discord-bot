986e2aa23f3f3dd27335c1a4aeb632ddea02d142
Multiconcepted <39141684+Multiconcepted@users.noreply.github.com>
2019-01-09 18:06:29 -0800

    Support subcommands

--- structures/command.js
+++ structures/command.js
@@ -15,18 +15,33 @@ class Command {
 		this.hidden = props.hidden || false;
 		this.nsfw = props.nsfw || false;
 		this.perms = props.perms || {bot: [], user: [], role: null, level: 0};
+		this.subcommands = [];
 		this.usage = props.usage || this.name;
 		
 		if (props.args) {
-			for (const arg of props.args) {this.args.push(new Argument(arg))}
+			for (const arg of props.args) this.args.push(new Argument(arg))
 		}
 		if (props.flags) {
-			for (const flag of props.flags) {this.flags.push(new Flag(flag))}
+			for (const flag of props.flags) this.flags.push(new Flag(flag))
+		}
+		if (props.subcommands) {
+			for (const scmd of props.subcommands) this.subcommands.push(new SubCommand(scmd))
 		}
 	}
 	
 	async run() {
 		throw new Error(`The command ${this.name} does not have a run() method`);
+	}
+}
+
+class SubCommand {
+	constructor(props) {
+		this.name = props.name;
+		this.args = [];
+
+		if (props.args) {
+			for (const arg of props.args) this.args.push(new Argument(arg))
+		}
 	}
 }
 
